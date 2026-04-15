5dfd8f409e7a7add672aca6f6af4ea66f38972e3
DiamondPlugg <dashalukashevich007@gmail.com>
2026-04-15 19:17:48 +0300

    3

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // 👇 ЗАМЕНИТЕ 'my-repo' на название вашего репозитория
-  base: process.env.NODE_ENV === 'production' ? '/BLAS02/' : '/',
+  base: '/BLAS02/',  // ← Имя вашего репозитория
 })
